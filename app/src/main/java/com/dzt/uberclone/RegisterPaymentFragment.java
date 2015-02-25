63ed81cff7f04bbe5d11c687c69bad37b7845385
david-zhou <david.zhou.tan@gmail.com>
2015-02-24 21:47:34 -0600

    validation and url request

--- app/src/main/java/com/dzt/uberclone/RegisterPaymentFragment.java
+++ app/src/main/java/com/dzt/uberclone/RegisterPaymentFragment.java
@@ -1,6 +1,7 @@
 package com.dzt.uberclone;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,6 +10,12 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,8 +23,11 @@ import android.widget.Toast;
 public class RegisterPaymentFragment extends Fragment implements View.OnClickListener{
 
     View v;
-    public RegisterPaymentFragment() {
-        // Required empty public constructor
+    String name, lastname, email, country, phone, password;
+
+    public RegisterPaymentFragment()
+    {
+
     }
 
 
@@ -33,6 +43,29 @@ public class RegisterPaymentFragment extends Fragment implements View.OnClickLis
         return v;
     }
 
+    private class URLpetition extends AsyncTask<String, Void, String>
+    {
+        @Override
+        protected String doInBackground(String... params) {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.interrupted();
+                }
+            }
+            return "Executed";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+        }
+
+        @Override
+        protected void onPreExecute() {}
+    }
+
     public void onClick(View v)
     {
         switch(v.getId())
@@ -40,7 +73,54 @@ public class RegisterPaymentFragment extends Fragment implements View.OnClickLis
             default:
             case R.id.registerPayment_registerCard:
                 //TODO validate card
-                Toast.makeText(getActivity(),"Validating card",Toast.LENGTH_SHORT).show();
+                Bundle args = getArguments();
+                name = args.getString("name");
+                lastname = args.getString("lastname");
+                email = args.getString("email");
+                phone = args.getString("phone");
+                password = args.getString("password");
+                country = args.getString("country");
+
+                try {
+                    StringBuilder sb = new StringBuilder("");
+                    StringBuilder jsonResults = new StringBuilder();
+
+                    sb.append("https://127.0.0.1:8000/users/register?email=");
+                    sb.append(email);
+                    sb.append("&name=");
+                    sb.append(name);
+                    sb.append("&last_name=");
+                    sb.append(lastname);
+                    sb.append("&password=");
+                    sb.append(password);
+                    sb.append("&phone=");
+                    sb.append(phone);
+                    sb.append("&country=");
+                    sb.append(country);
+
+
+                    URL url = new URL(sb.toString());
+                    HttpURLConnection conn = null;
+                    conn = (HttpURLConnection) url.openConnection();
+                    InputStreamReader in = new InputStreamReader(conn.getInputStream());
+                    int read;
+                    char[] buff = new char[1024];
+                    while ((read = in.read(buff)) != -1) {
+                        jsonResults.append(buff, 0, read);
+                    }
+                    Toast.makeText(getActivity(), jsonResults.toString(),Toast.LENGTH_SHORT).show();
+                }
+                catch (MalformedURLException e)
+                {
+                    e.printStackTrace();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+
+
+                //Toast.makeText(getActivity(),name + lastname + email + phone + password + country,Toast.LENGTH_SHORT).show();
                 break;
         }
     }
--- app/src/main/java/com/dzt/uberclone/SignupFragment.java
+++ app/src/main/java/com/dzt/uberclone/SignupFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 
 /**
@@ -16,6 +18,9 @@ import android.widget.Button;
 public class SignupFragment extends Fragment implements View.OnClickListener{
 
     View v;
+    EditText name, lastname, email, phone, password;
+    Spinner country;
+
     public SignupFragment() {
         // Required empty public constructor
     }
@@ -30,6 +35,13 @@ public class SignupFragment extends Fragment implements View.OnClickListener{
         Button next = (Button) v.findViewById(R.id.signup_next);
         next.setOnClickListener(this);
 
+        name = (EditText) v.findViewById(R.id.signup_name);
+        lastname = (EditText) v.findViewById(R.id.signup_lastname);
+        email = (EditText) v.findViewById(R.id.signup_email);
+        phone = (EditText) v.findViewById(R.id.signup_phone);
+        password = (EditText) v.findViewById(R.id.signup_password);
+        country = (Spinner) v.findViewById(R.id.signup_country);
+
         return v;
     }
 
@@ -40,16 +52,90 @@ public class SignupFragment extends Fragment implements View.OnClickListener{
             default:
             case R.id.signup_next:
                 //TODO validate fields
+                if(validate_signup_fields())
+                {
+                    Fragment fragment = new RegisterPaymentFragment();
 
-                Fragment fragment = new RegisterPaymentFragment();
-                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                fragmentManager.beginTransaction()
-                        .replace(R.id.login_container, fragment).addToBackStack("registerCard")
-                        .commit();
+                    Bundle args = new Bundle();
+                    args.putString("name", name.getText().toString().trim());
+                    args.putString("lastname", lastname.getText().toString().trim());
+                    args.putString("email", email.getText().toString().trim());
+                    args.putString("phone", phone.getText().toString().trim());
+                    args.putString("password", password.getText().toString().trim());
+                    args.putString("country", country.getSelectedItem().toString().trim());
 
-                //next fragment
+                    fragment.setArguments(args);
+
+                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.login_container, fragment).addToBackStack("registerCard")
+                            .commit();
+
+                    //next fragment
+                }
                 break;
         }
+    }
+
+    boolean validate_signup_fields()
+    {
+        boolean valid = true;
+        if(name.getText().toString().trim().equals(""))
+        {
+            name.setError("Name must not be empty");
+            valid = false;
+        }
+        else
+        {
+            name.setError(null);
+        }
+
+        if(lastname.getText().toString().trim().equals(""))
+        {
+            lastname.setError("Last name must not be empty");
+            valid = false;
+        }
+        else
+        {
+            lastname.setError(null);
+        }
+
+        if(email.getText().toString().trim().equals(""))
+        {
+            email.setError("Email must not be empty");
+            valid = false;
+        }
+        else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email.getText()).matches())
+        {
+            email.setError("Enter a valid email address");
+            valid = false;
+        }
+        else
+        {
+            email.setError(null);
+        }
+
+        if(phone.getText().toString().trim().length() < 10)
+        {
+            phone.setError("Enter a 10 digit phone number");
+            valid = false;
+        }
+        else
+        {
+            phone.setError(null);
+        }
+
+        if(password.getText().toString().trim().equals(""))
+        {
+            password.setError("Password can't be empty");
+            valid = false;
+        }
+        else
+        {
+            password.setError(null);
+        }
+
+        return valid;
     }
 
 }
