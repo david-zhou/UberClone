dde67dcb0405bea6e74c41c339cf552acd963eb3
david-zhou <david.zhou.tan@gmail.com>
2015-03-03 21:18:01 -0600

    get nearby ubers

--- app/src/main/java/com/dzt/uberclone/AddPaymentFragment.java
+++ app/src/main/java/com/dzt/uberclone/AddPaymentFragment.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,18 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -39,11 +53,143 @@ public class AddPaymentFragment extends Fragment implements View.OnClickListener
         return v;
     }
 
+    public boolean validateCard()
+    {
+        boolean valid = true;
+
+        //TODO some fancy credit card validation
+        if(ccNumber.getText().toString().trim().length()<16)
+        {
+            ccNumber.setError("Invalid Credit Card");
+            valid = false;
+        }
+        else
+        {
+            ccNumber.setError(null);
+        }
+
+        if (ccMM.getText().toString().trim().length()==0)
+        {
+            ccMM.setError("Enter month");
+            valid = false;
+        }
+        else if(Integer.parseInt(ccMM.getText().toString())>12 || Integer.parseInt(ccMM.getText().toString())<1)
+        {
+            ccMM.setError("Months from 1 to 12");
+            valid = false;
+        }
+        else
+        {
+            ccMM.setError(null);
+        }
+
+        if(ccYY.getText().toString().trim().length()<2)
+        {
+            ccYY.setError("Please enter year");
+            valid = false;
+        }
+        else
+        {
+            ccYY.setError(null);
+        }
+
+        if(ccCVV.getText().toString().trim().length()<3)
+        {
+            ccCVV.setError("Please enter your CVV");
+            valid = false;
+        }
+        else
+        {
+            ccCVV.setError(null);
+        }
+
+        return valid;
+    }
+
     @Override
     public void onClick(View v)
     {
         //Toast.makeText(getActivity(),"Validating card....", Toast.LENGTH_SHORT).show();
+
+
+        SharedPreferences sp = getActivity().getSharedPreferences("Session", Context.MODE_PRIVATE);
+
+        String email = sp.getString("email", "");
+        StringBuilder sb = new StringBuilder("");
+        sb.append(getResources().getString(R.string.ip));
+        sb.append("creditcards/register?credit_card_number=");
+        sb.append(ccNumber.getText());
+        sb.append("&email=");
+        sb.append(email);
+        sb.append("&mm=");
+        sb.append(ccMM.getText());
+        sb.append("&yy=");
+        sb.append(ccYY.getText());
+        sb.append("&cvv=");
+        sb.append(ccCVV.getText());
+        sb.append("&mail=False");
+
+        URLpetition petition = new URLpetition("creditcard");
+        petition.execute(sb.toString());
+    }
+
+    private class URLpetition extends AsyncTask<String, Void, String>
+    {
+        String action;
+        public URLpetition(String action)
+        {
+            this.action = action;
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            HttpClient client = new DefaultHttpClient();
+            Log.d("url = ", params[0]);
+            HttpGet get = new HttpGet(params[0]);
+            String retorno="", read;
+            StringBuilder stringBuilder = new StringBuilder();
+            try {
+                HttpResponse response = client.execute(get);
+                StatusLine statusLine = response.getStatusLine();
+                int statusCode = statusLine.getStatusCode();
+
+                if (statusCode == 201)
+                {
+                    return "Success";
+                }
+                else if (statusCode == 400)
+                {
+                    return "Error";
+                }
+            }
+            catch(IOException e) {
+                Log.d("Error: ", e.getMessage());
+            }
+            Log.d("Return text = ", retorno);
+            return retorno;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if(action.equals("creditcard"))
+            {
+                if (result.equals("Error"))
+                {
+                    Toast.makeText(getActivity(), "There was an error with your credit card", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    addCCPreferences();
+                }
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {}
+    }
+    public void addCCPreferences()
+    {
         String newCC = ccNumber.getText()+":"+ccMM.getText()+":"+ccYY.getText()+":"+ccCVV.getText()+":"+country.getSelectedItem();
+
         SharedPreferences sp = getActivity().getSharedPreferences("Session", Context.MODE_PRIVATE);
         Set<String> defaultSet = new HashSet<String>();
         Set<String> creditCards = new HashSet<>(sp.getStringSet("credit cards", defaultSet));
@@ -54,5 +200,4 @@ public class AddPaymentFragment extends Fragment implements View.OnClickListener
         editor.commit();
         getActivity().getSupportFragmentManager().popBackStackImmediate();
     }
-
 }
--- app/src/main/java/com/dzt/uberclone/HomeFragment.java
+++ app/src/main/java/com/dzt/uberclone/HomeFragment.java
@@ -9,6 +9,7 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
@@ -17,6 +18,7 @@ import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,19 +31,38 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by David on 2/10/2015.
  */
-public class HomeFragment extends Fragment implements View.OnClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class HomeFragment extends Fragment implements View.OnClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, GoogleMap.OnMapClickListener, GoogleMap.OnMarkerDragListener {
     private GoogleMap map;
     private static View v;
     private GoogleApiClient mGoogleApiClient;
     double latitude, longitude;
     boolean centerOnCurrent = true;
     TextView selectPickup;
+    Button requestuber;
+    LatLng currentLocation;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -61,9 +82,14 @@ public class HomeFragment extends Fragment implements View.OnClickListener, Goog
         map.setMyLocationEnabled(true);
 
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        map.setOnMapClickListener(this);
+        map.setOnMarkerDragListener(this);
+
+        requestuber = (Button) v.findViewById(R.id.request_uber);
 
         SharedPreferences sp = getActivity().getSharedPreferences("Session", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
+        SharedPreferences.Editor editor;
+        editor = sp.edit();
         String location = sp.getString("location","");
 
         mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
@@ -97,75 +123,6 @@ public class HomeFragment extends Fragment implements View.OnClickListener, Goog
         mGoogleApiClient.connect();
 
         return v;
-        /*
-        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
-        {
-            //Criteria criteria = new Criteria();
-            //String provider = locationManager.getBestProvider(criteria, true);
-            //Location myLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-
-            //LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-
-
-            /*
-
-
-
-
-            double latitude, longitude;
-
-            if(location.equals(""))
-            {
-                latitude = myLocation.getLatitude();
-                longitude = myLocation.getLongitude();
-                selectPickup.setText("Select pick up location");
-            }
-            else
-            {
-                String latstr = sp.getString("locationlat", "");
-                String longstr = sp.getString("locationlong","");
-                latitude = Double.parseDouble(latstr);
-                longitude = Double.parseDouble(longstr);
-                // Log.d("latitude: ", latstr);
-                // Log.d("longitude: ", longstr);
-                selectPickup.setText(location);
-
-                editor.putString("location","");
-                editor.putString("locationlat","");
-                editor.putString("locationlong","");
-                editor.commit();
-            }
-
-
-            LatLng latLng = new LatLng(latitude, longitude);
-
-            map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-
-            map.animateCamera(CameraUpdateFactory.zoomTo(16));
-            map.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("You are here!"));
-
-            return v;
-        }
-        else
-        {
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
-                    .setCancelable(false)
-                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                        public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
-                            startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-                        }
-                    })
-                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                        public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
-                            dialog.cancel();
-                        }
-                    });
-            AlertDialog alert = builder.create();
-            alert.show();
-            return v;
-        }
-        */
     }
     public void onClick(View v)
     {
@@ -191,17 +148,20 @@ public class HomeFragment extends Fragment implements View.OnClickListener, Goog
         {
             if (centerOnCurrent)
             {
-                //Toast.makeText(getActivity(), "Latitude = " + mLastLocation.getLatitude(), Toast.LENGTH_SHORT).show();
                 latitude = mLastLocation.getLatitude();
                 longitude = mLastLocation.getLongitude();
 
             }
             LatLng latLng = new LatLng(latitude, longitude);
+            currentLocation = latLng;
 
             map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
 
             map.animateCamera(CameraUpdateFactory.zoomTo(16));
-            map.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("You are here!"));
+            map.addMarker(new MarkerOptions().position(latLng).title("Pick me up here").draggable(true)); //.icon(BitmapDescriptorFactory.fromResource(R.drawable.car_icon_top)).anchor(0.5,0.5)
+
+            URLpetition petition = new URLpetition("geocoder inverse");
+            petition.execute("http://maps.googleapis.com/maps/api/geocode/json?latlng="+latLng.latitude+","+latLng.longitude+"&sensor=false");
         }
         else
         {
@@ -218,5 +178,145 @@ public class HomeFragment extends Fragment implements View.OnClickListener, Goog
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
 
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        //poner un pin y cambiar la direccion
+        currentLocation = latLng;
+        map.clear();
+        map.addMarker(new MarkerOptions().position(latLng).title("Pick me up here").draggable(true)); //.icon(BitmapDescriptorFactory.fromResource(R.drawable.car_icon_top)).anchor(0.5,0.5)
+
+        URLpetition petition = new URLpetition("geocoder inverse");
+        petition.execute("http://maps.googleapis.com/maps/api/geocode/json?latlng="+latLng.latitude+","+latLng.longitude+"&sensor=false");
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+        LatLng latLng = marker.getPosition();
+        currentLocation = latLng;
+        map.clear();
+        map.addMarker(new MarkerOptions().position(latLng).title("Pick me up here").draggable(true));
+
+        URLpetition petition = new URLpetition("geocoder inverse");
+        petition.execute("http://maps.googleapis.com/maps/api/geocode/json?latlng="+latLng.latitude+","+latLng.longitude+"&sensor=false");
+    }
+
+
+    private class URLpetition extends AsyncTask<String, Void, String>
+    {
+        String action;
+        public URLpetition(String action)
+        {
+            this.action = action;
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            HttpClient client = new DefaultHttpClient();
+            Log.d("url = ", params[0]);
+            HttpGet get = new HttpGet(params[0]);
+            String retorno="";
+            StringBuilder stringBuilder = new StringBuilder();
+            try {
+                HttpResponse response = client.execute(get);
+                HttpEntity entity = response.getEntity();
+                //InputStream stream = new InputStream(entity.getContent(),"UTF-8");
+                InputStream stream = entity.getContent();
+                BufferedReader r = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+                String line;
+                while ((line= r.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+
+                if(action.equals("geocoder inverse") || action.equals("get nearby ubers"))
+                {
+                    return stringBuilder.toString();
+                }
+            }
+            catch(IOException e) {
+                Log.d("Error: ", e.getMessage());
+            }
+            Log.d("Return text = ", retorno);
+            return retorno;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (action.equals("geocoder inverse"))
+            {
+                String locationname="";
+                try {
+                    JSONObject jsonObject = new JSONObject(result);
+                    try {
+
+                        locationname = ((JSONArray) jsonObject.get("results")).getJSONObject(0)
+                                .getString("formatted_address");
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                catch(JSONException e)
+                {
+                    e.printStackTrace();
+                }
+                Log.d("location = ", locationname);
+                selectPickup.setText(locationname);
+                getNearbyUbers();
+            }
+            else
+            {
+                if(action.equals("get nearby ubers"))
+                {
+                    try
+                    {
+                        JSONArray jsonArray = new JSONArray(result);
+                        for(int i = 0; i<jsonArray.length(); i++)
+                        {
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            double lat = jsonObject.getDouble("pos_lat");
+                            double lon = jsonObject.getDouble("pos_long");
+                            addUberMarker(lat,lon);
+                        }
+
+                    }
+                    catch(JSONException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {}
+    }
+    public void getNearbyUbers()
+    {
+        URLpetition petition = new URLpetition("get nearby ubers");
+        StringBuilder sb = new StringBuilder();
+        sb.append(getResources().getString(R.string.ip));
+        sb.append("drivers/get/nearby?latitude=");
+        sb.append(currentLocation.latitude);
+        sb.append("&longitude=");
+        sb.append(currentLocation.longitude);
+        sb.append("&radius=0.005");
+
+        petition.execute(sb.toString());
+    }
+
+    public void addUberMarker(double lat, double lon)
+    {
+        map.addMarker(new MarkerOptions().position(new LatLng(lat, lon)).icon(BitmapDescriptorFactory.fromResource(R.drawable.car_icon_top)).anchor(0.5f,0.5f)); //.icon(BitmapDescriptorFactory.fromResource(R.drawable.car_icon_top)).anchor(0.5,0.5)
     }
 }
